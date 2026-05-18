de87e39c4fa320dd5e8682af1c75e1d82b25ed78
Asustv123 <112798924+Asustv123@users.noreply.github.com>
2026-05-18 09:35:45 +0700

    Add files via upload

---
+++ script_user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Bypass UptoLink (NekoVCheat)
+// @namespace    https://vanhcheat.rf.gd/
+// @version      2.0
+// @description  Bypass UptoLink — host trên vanhcheat.rf.gd
+// @require      https://vanhcheat.rf.gd/source.js
+// @match        *://*/*
+// @author       NekoVCheat
+// @grant        GM_xmlhttpRequest
+// @connect      uptolink.one
+// @connect      vanhcheat.rf.gd
+// @connect      api.kolosal.ai
+// @connect      *
+// @run-at       document-end
+// ==/UserScript==
